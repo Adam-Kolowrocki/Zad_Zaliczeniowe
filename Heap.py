38c9f5e4cc3732fe5f948182c1dc3bcf531126a2
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-07-09 18:16:56 +0200

    .

---
+++ Heap.py
@@ -0,0 +1,38 @@
+numbers = [13, 24, 25, 26, 27, 15, 35, 36, 29, 49, 41, 37, 45, 68, 75, 25, 16]
+
+
+def heap(numbers, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and numbers[i] < numbers[l]:
+        largest = l
+
+    if r < n and numbers[largest] < numbers[r]:
+        largest = r
+
+    if largest != i:
+        numbers[i], numbers[largest] = numbers[largest], numbers[i]
+
+        heap(numbers, n, largest)
+
+
+def heap_sort(numbers):
+    n = len(numbers)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heap(numbers, n, i)
+
+    for i in range(n - 1, 0, -1):
+        numbers[i], numbers[0] = numbers[0], numbers[i]
+        heap(numbers, i, 0)
+
+
+def main():
+    heap_sort(numbers)
+    print(f' Sorted numbers: {numbers} ')
+
+
+if __name__ == "__main__":
+    main()
