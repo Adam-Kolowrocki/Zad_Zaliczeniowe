41dc2b54388f814e701118f400d1cd568f122de4
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-07-09 22:37:07 +0200

    .

--- sorting.py
+++ sorting.py
@@ -2,27 +2,35 @@ import random
 clear = '\n'*25
 
 
-def data_input(source_choice):
-    source_choice = menu()
+def data_input():
+    source_choice = 'k'
     if source_choice == 'f':
-        with open('naturals_list.txt') as f:
-            data_list = f.readline()
-            return data_list
+        with open('n_numbers.txt') as f:
+            numbers = f.readline()
+            numbers = numbers.rsplit(", ")
+            for i in range(0, len(numbers)):
+                numbers[i] = int(numbers[i])
+            return numbers
     elif source_choice == 'r':
-        data_list = []
-        for i in range(0, 9):
-            data_list.append(random.randint(10, 99))
-            return data_list
+        numbers = []
+        for i in range(0, 30):
+            n = random.randint(10, 99)
+            numbers.append(n)
+        return numbers
     elif source_choice == 'k':
-        data_list = []
-        print(f'Type 10 natural numbers.')
-        while len(data_list) < 10:
-            user_input = int(input(f'Type a number ->'))
-            if user_input > 0 and user_input // user_input == 0:
-                data_list.append(user_input)
-            else:
-                print(f'Wrong number... Type natural numbers.')
-        return data_list
+        while True:
+            numbers = []
+            num_count = int(input(f'How many natural numbers do You want to sort ?'))
+            if num_count <= 0:
+                print(f'Wrong number... Type natural number.')
+                continue
+            for i in range(1, num_count + 1):
+                user_input = int(input(f'Type {i} number ->'))
+                if user_input > 0:
+                    numbers.append(user_input)
+                else:
+                    print(f'Wrong number... Type natural numbers.')
+            return numbers
 
 
 def menu():
