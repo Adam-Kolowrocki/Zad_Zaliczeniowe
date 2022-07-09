aeb72a97ddadb8f2ba7039b94bc9988e924e268d
Adam Kołowrocki <adamkolowrocki@gmail.com>
2022-07-09 16:28:59 +0200

    .

--- Merge.py
+++ Merge.py
@@ -2,18 +2,33 @@ numbers = [13, 24, 25, 26, 27, 15, 35, 36, 29, 49, 41, 37, 45, 68, 75, 25, 16]
 
 
 def mergesort(numbers):
-    if len(numbers) == 1:
-        return
-    while len(numbers) > 1:
-        l_idx = 0
-        r_idx = len(numbers) - 1
-        mid_idx = (l_idx + r_idx) // 2
-        left = numbers[:mid_idx]
-        right = numbers[mid_idx:]
+    if len(numbers) > 1:
+        left_numbers = numbers[:len(numbers) // 2]
+        right_numbers = numbers[len(numbers) // 2:]
 
-        print(left)
-        print(right)
+        # recursion
+        mergesort(left_numbers)
+        mergesort(right_numbers)
 
+        #merge
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_numbers) and j < len(right_numbers):
+            if left_numbers[i] < right_numbers[j]:
+                numbers[k] = left_numbers[i]
+                i += 1
+            else:
+                numbers[k] = right_numbers[j]
+                j += 1
+            k += 1
+        while i < len(left_numbers):
+            numbers[k] = left_numbers[i]
+            i += 1
+        while j < len(right_numbers):
+            numbers[k] = right_numbers[j]
+            j += 1
+    return numbers
 
 
 def main():
---
+++ Testowe/Testy_merge_01.py
@@ -0,0 +1,28 @@
+numbers = [13, 24, 25, 26, 27, 15, 35, 36, 29, 49, 41, 37, 45, 68, 75, 25, 16]
+
+
+def mergesort(numbers):
+    if len(numbers) == 1:
+        return
+    while len(numbers) > 1:
+        l_idx = 0
+        r_idx = len(numbers) - 1
+        mid_idx = (l_idx + r_idx) // 2
+        left = numbers[:mid_idx]
+        right = numbers[mid_idx:]
+
+        print(left)
+        print(right)
+
+
+def main():
+    print('Given array is:', end="\n")
+    print(numbers)
+    print(type(numbers))
+    print('Sorted array is:', end="\n")
+    print(mergesort(numbers))
+    print(type(mergesort(numbers)))
+
+
+if __name__ == "__main__":
+    main()
